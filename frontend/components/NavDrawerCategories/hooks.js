37d2ce112022db9add08af1973c04998a211d4ff
PMank <philipp.mank@shopgate.com>
2022-08-03 07:20:00 +0200

    removed

--- frontend/components/NavDrawerCategories/hooks.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { Context } from './Provider';
-
-/**
- * The useCategoriesContext hook
- * @returns {Function}
- */
-export const useCategoriesContext = () => useContext(Context);

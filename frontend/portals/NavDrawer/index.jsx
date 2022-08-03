e6a8e13494aefd2298946c41ca25f4e3590c0cae
PMank <philipp.mank@shopgate.com>
2022-08-03 07:21:57 +0200

    added ios check

--- frontend/portals/NavDrawer/index.jsx
+++ frontend/portals/NavDrawer/index.jsx
@@ -3,15 +3,24 @@ import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
 import { NavDrawer } from '@shopgate/pwa-ui-material';
 import NavDrawerCategoriesTree from '../../components/NavDrawerCategoriesTree';
+import { themeName } from '@shopgate/pwa-common/helpers/config';
+
+const isIOS = themeName.includes('ios');
 
 /**
  * @returns {JSX}
  */
-const NavDrawerContainer = ({ onOpen, onClose }) => (
-  <NavDrawer onOpen={onOpen} onClose={onClose}>
-    <NavDrawerCategoriesTree />
-  </NavDrawer>
-);
+const NavDrawerContainer = ({ onOpen, onClose }) => {
+  if (!isIOS) {
+    return null;
+  }
+
+  return (
+    <NavDrawer onOpen={onOpen} onClose={onClose}>
+      <NavDrawerCategoriesTree />
+    </NavDrawer>
+  );
+};
 
 NavDrawerContainer.propTypes = {
   onClose: PropTypes.func,

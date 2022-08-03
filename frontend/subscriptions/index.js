6f7ca33672bede43767afe5186ef4b18fa526c7b
PMank <philipp.mank@shopgate.com>
2022-08-03 07:22:48 +0200

    removed event listener

--- frontend/subscriptions/index.js
+++ frontend/subscriptions/index.js
@@ -2,11 +2,6 @@ import { appWillStart$ } from '@shopgate/pwa-common/streams/app';
 import { fetchCategoryTree } from '../actions';
 
 export default (subscribe) => {
-  let windowReady = false;
-  window.addEventListener('load', () => {
-    windowReady = true;
-  });
-
   subscribe(appWillStart$, ({ dispatch }) => {
     dispatch(fetchCategoryTree());
   });          

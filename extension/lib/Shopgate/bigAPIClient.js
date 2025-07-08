886193535aedd481d5f5a5049a786afbcbd32a2b
Martin Sandrock <martin.sandrock@shopgate.com>
2025-07-08 12:08:06 +0200

    CURB-5131 - Support catalog service requests

--- extension/lib/Shopgate/bigAPIClient.js
+++ extension/lib/Shopgate/bigAPIClient.js
@@ -1,6 +1,30 @@
 const errors = require('request-promise-native/errors')
+const config = require('../../config.json')
 
 const LIMIT = 100
+
+/**
+ * Map a catalog service category to the old product service format to ensure compatibility
+ * @param {Object} category
+ * @private
+ */
+const _mapCatalogCategoryToProductCategory = category => ({
+  id: category.code,
+  name: category.name,
+  path: category.path,
+  parent: {
+    id: category.parentCategoryCode || null,
+    name: '' // Not known here
+  },
+  description: category.description,
+  imageUrl: category.image,
+  externalUrl: null,
+  sort: category.sequenceId,
+  productCount: category.productCount,
+  childrenSort: 'manual',
+  childrenCount: category.categoryCount,
+  children: []
+})
 
 module.exports = class {
   /**
@@ -21,27 +45,48 @@ module.exports = class {
   }
 
   async recursiveFetchCategories (resolve, reject, context, offset = 0, results = []) {
-    const path = `${context.meta.appId.split('_')[1]}/categorySearch?limit=${LIMIT}&offset=${offset}&children=false`
     try {
-      const { body: response } = await this.request(
-        'product',
-        'v1',
-        path,
-        'GET'
-      ) || {}
+      if (config.useCatalogService) {
+        const { body: catalogResponse } = await this.request(
+          'catalog',
+          'v1',
+          `/merchants/${config.omniCredentials.tenantId}/categories?limit=${LIMIT}&offset=${offset}`,
+          'GET'
+        )
 
-      const { collection = [], meta } = response || {}
-      const { pagination } = meta || {}
-      const { totalItemCount } = pagination || {}
+        const { categories, meta: { totalItemCount } } = catalogResponse
+        // Map categories from the catalog service format to the expected legacy product service format
+        const mappedCategories = categories.map(_mapCatalogCategoryToProductCategory)
+        const newResults = [...results, ...mappedCategories]
 
-      const newResults = [...results, ...collection]
+        if (newResults.length < totalItemCount) {
+          this.recursiveFetchCategories(resolve, reject, context, offset + LIMIT, newResults)
+          return
+        }
 
-      if (newResults.length < totalItemCount) {
-        this.recursiveFetchCategories(resolve, reject, context, offset + LIMIT, newResults)
-        return
+        return resolve(newResults)
+      } else {
+        const path = `${context.meta.appId.split('_')[1]}/categorySearch?limit=${LIMIT}&offset=${offset}&children=false`
+        const { body: response } = await this.request(
+          'product',
+          'v1',
+          path,
+          'GET'
+        ) || {}
+
+        const { collection = [], meta } = response || {}
+        const { pagination } = meta || {}
+        const { totalItemCount } = pagination || {}
+
+        const newResults = [...results, ...collection]
+
+        if (newResults.length < totalItemCount) {
+          this.recursiveFetchCategories(resolve, reject, context, offset + LIMIT, newResults)
+          return
+        }
+
+        resolve(newResults)
       }
-
-      resolve(newResults)
     } catch (err) {
       context.log.error(err)
       reject(err)
